fc0f9fc39c75d35bf343c98a404706b497c055ee
Tatum Sterling <tatum.sterling29@gmail.com>
2024-01-20 00:50:41 -0600

    ran create vite project and added my starter code

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,23 @@
+
+
+const styles = {
+  headerStyle: {
+    background: 'red',
+  },
+  headingStyle: {
+    fontSize: '100px',
+  },
+};
+
+// We use JSX curly braces to evaluate the style object
+
+function Header() {
+  return (
+    <header style={styles.headerStyle} className="header">
+      <h1 style={styles.headingStyle}>Welcome</h1>
+    </header>
+  );
+}
+
+export default Header;
+
